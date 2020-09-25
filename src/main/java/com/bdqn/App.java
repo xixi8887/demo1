bbbf1ad81926321364fc313f0facd103f59bb746
xixi8887 <1762724568@qq.com>
2020-09-25 15:32:35 +0800

    更新1

--- src/main/java/com/bdqn/App.java
+++ src/main/java/com/bdqn/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("创建本地工作区");
         System.out.println("将项目提交到暂存区");
+        System.out.println("更新1");
     }
 }
